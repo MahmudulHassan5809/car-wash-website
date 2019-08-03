a3d333002672d77b52e1e10fd50e46b0f7db9580
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2019-08-03 23:15:06 +0600

    New Database

--- car_wash_app.sql
+++ car_wash_app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 02, 2019 at 08:36 PM
+-- Generation Time: Aug 03, 2019 at 07:14 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.2
 
