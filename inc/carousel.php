183eb8e073fcc0fc4980cda309844b20614530fc
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2019-08-03 19:57:22 +0600

    Change Slider

--- inc/carousel.php
+++ inc/carousel.php
@@ -1,125 +1,40 @@
-<!--Carousel Wrapper-->
-<div id="carousel-example-1z" class="carousel slide carousel-fade" data-ride="carousel">
-
-    <!--Indicators-->
+<header>
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
-    <li data-target="#carousel-example-1z" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-1z" data-slide-to="1"></li>
-    <li data-target="#carousel-example-1z" data-slide-to="2"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
     </ol>
-    <!--/.Indicators-->
-
-    <!--Slides-->
     <div class="carousel-inner" role="listbox">
-
-        <!--First slide-->
-        <div class="carousel-item active">
-            <div class="view" style="background-image: url('assets/img/carosuel/car_wash1.jpg'); background-repeat: no-repeat; background-size: cover;">
-
-            <!-- Mask & flexbox options-->
-            <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
-
-            <!-- Content -->
-            <div class="text-center white-text mx-5 wow fadeIn">
-                <h1 class="mb-4">
-                <strong>Choose Your Service From Our Site</strong>
-                </h1>
-
-                <p>
-                <strong>Best & Resonable Price</strong>
-                </p>
-
-                <p class="mb-4 d-none d-md-block">
-                <strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam dicta, nemo iste obcaecati vitae dignissimos odio expedita est labore in deleniti quibusdam ab, itaque hic corrupti ratione mollitia, minus voluptas!</strong>
-                </p>
-
-            </div>
-            <!-- Content -->
-
-            </div>
-            <!-- Mask & flexbox options-->
-
-            </div>
+      <!-- Slide One - Set the background image for this slide in the line below -->
+      <div class="carousel-item active" style="background-image: url('assets/img/carosuel/car_wash1.jpg')">
+        <div class="carousel-caption d-none d-md-block">
+          <h3 class="display-4">Repair & Wash Your Car</h3>
+          <p class="lead">Best Services & Resonable Price</p>
         </div>
-        <!--/First slide-->
-
-        <!--Second slide-->
-        <div class="carousel-item">
-            <div class="view" style="background-image: url('assets/img/carosuel/car_wash2.jpg'); background-repeat: no-repeat; background-size: cover;">
-
-                <!-- Mask & flexbox options-->
-                <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
-
-                    <!-- Content -->
-                    <div class="text-center white-text mx-5 wow fadeIn">
-                        <h1 class="mb-4">
-                        <strong>Choose Your Service From Our Site</strong>
-                        </h1>
-
-                        <p>
-                        <strong>Best & Resonable Price</strong>
-                        </p>
-
-                        <p class="mb-4 d-none d-md-block">
-                        <strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae provident ipsam iste beatae minus totam nam dignissimos aliquam autem consectetur dolorum officia quis, temporibus, ratione necessitatibus nesciunt, alias voluptates saepe!</strong>
-                        </p>
-
-
-                    </div>
-                    <!-- Content -->
-
-                </div>
-                <!-- Mask & flexbox options-->
-
-            </div>
+      </div>
+      <!-- Slide Two - Set the background image for this slide in the line below -->
+      <div class="carousel-item" style="background-image: url('assets/img/carosuel/car_wash2.jpg')">
+        <div class="carousel-caption d-none d-md-block">
+          <h3 class="display-4">Repair & Wash Your Car</h3>
+          <p class="lead">Best Services & Resonable Price</p>
         </div>
-        <!--/Second slide-->
-
-       <!--First slide-->
-        <div class="carousel-item">
-            <div class="view" style="background-image: url('assets/img/carosuel/car_wash3.jpg'); background-repeat: no-repeat; background-size: cover;">
-
-            <!-- Mask & flexbox options-->
-            <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
-
-            <!-- Content -->
-            <div class="text-center white-text mx-5 wow fadeIn">
-                <h1 class="mb-4">
-                <strong>Choose Your Service From Our Site</strong>
-                </h1>
-
-                <p>
-                <strong>Best & Resonable Price</strong>
-                </p>
-
-                <p class="mb-4 d-none d-md-block">
-                <strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae provident ipsam iste beatae minus totam nam dignissimos aliquam autem consectetur dolorum officia quis, temporibus, ratione necessitatibus nesciunt, alias voluptates saepe!</strong>
-                </p>
-
-
-            </div>
-            <!-- Content -->
-
-            </div>
-            <!-- Mask & flexbox options-->
-
-            </div>
+      </div>
+      <!-- Slide Three - Set the background image for this slide in the line below -->
+      <div class="carousel-item" style="background-image: url('assets/img/carosuel/car_wash3.jpg')">
+        <div class="carousel-caption d-none d-md-block">
+          <h3 class="display-4">Repair & Wash Your Car</h3>
+          <p class="lead">Best Services & Resonable Price</p>
         </div>
-        <!--/First slide-->
-
+      </div>
     </div>
-    <!--/.Slides-->
-
-    <!--Controls-->
-    <a class="carousel-control-prev" href="#carousel-example-1z" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carousel-example-1z" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-    </a>
-    <!--/.Controls-->
-
-</div>
-<!--/.Carousel Wrapper-->
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <i class="fas fa-arrow-left"></i>
+          <span class="sr-only">Previous</span>
+        </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <i class="fas fa-arrow-right"></i>
+          <span class="sr-only">Next</span>
+        </a>
+  </div>
+</header>
